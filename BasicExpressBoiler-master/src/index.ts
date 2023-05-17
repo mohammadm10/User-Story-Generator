519e915d02b4c62eaa11a8c592905ddacf4702d0
mohammadm10 <mohammadali_10@hotmail.com>
2023-05-16 21:21:41 -0700

    added api key

--- BasicExpressBoiler-master/src/index.ts
+++ BasicExpressBoiler-master/src/index.ts
@@ -1,11 +1,45 @@
 import express from 'express';
+import cors from 'cors';
+import fetch from 'isomorphic-fetch';
+import { config } from './config';
 
 const app = express();
 const port = 3000;
 
-app.get('/api', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.json({ message: 'Hello from the backend!' });
+app.use(cors());
+
+app.get('/api', async (req, res) => {
+  const prompt = 'Give me a user story in the Given, When, Then format for this requirement: User should be able to delete an account';
+  const apiKey = config.apiKey
+  const model = 'gpt-3.5-turbo';
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        model,
+        messages: [{ role: 'system', content: prompt }],
+      }),
+    });
+
+    const data = await response.json();
+    console.log('API Response:', data);
+
+    if (data.choices && data.choices.length > 0) {
+      const message = data.choices[0].message.content.trim();
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ message });
+    } else {
+      res.status(500).json({ error: 'Unexpected response from OpenAI API' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
 
 app.listen(port, () => {
