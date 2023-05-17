97e4797ec18a1596a26d1f8b0526240eec86b9f3
mohammadm10 <mohammadali_10@hotmail.com>
2023-05-17 13:59:44 -0700

    styling, loading, error handling

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -2,6 +2,10 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { TextField, Button } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { IconButton, Tooltip } from '@mui/material';
+import { CircularProgress } from '@mui/material';
+import Swal from 'sweetalert2';
 
 const darkTheme = createTheme({
   palette: {
@@ -9,12 +13,46 @@ const darkTheme = createTheme({
   },
 });
 
-function App() {
+const keywords = ['given', 'when', 'then'];
 
+function formatReplyWithKeywords(reply: string) {
+  const regex = new RegExp(`\\b(${keywords.join('|')})\\b`, 'gi');
+  const parts = reply.split(regex);
+
+  const formattedParts = parts.map((part, index) => {
+    if (keywords.includes(part.toLowerCase())) {
+      return (
+        <strong key={index} style={{ textTransform: 'uppercase' }}>
+          <br />
+          {part.replace(/\*/g, '')}
+        </strong>
+      );
+    } else {
+      return <span key={index}>{part}</span>;
+    }
+  });
+
+  return formattedParts;
+}
+
+
+function App() {
   const [reply, setReply] = useState('');
   const [input, setInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log(reply.length);
+
 
   const handleClick = () => {
+    if (input === '') {
+      Swal.fire({
+        icon: 'error',
+        title: 'Missing requirement',
+        text: 'Please enter a requirement in order to generate a User Story!',
+      })
+    }
+    setIsLoading(true);
     fetch(`http://localhost:3000/api/${input}`)
       .then((response) => response.json())
       .then((data) => setReply(data.message))
@@ -22,18 +60,22 @@ function App() {
   };
 
   const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(event.target.value)
-   }
+    setInput(event.target.value);
+  };
+
+  const onCopyClick = () => {
+    navigator.clipboard.writeText(reply);
+  };
 
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <main>
-        <h1 className=' text-4xl font-bold flex justify-center py-20'>Welcome to User Story Generator!</h1>
-        <div className=' flex justify-center'>
+        <h1 className='text-4xl font-bold flex justify-center py-20'>Welcome to User Story Generator!</h1>
+        <div className='flex justify-center'>
           <TextField
             sx={{
-              width: { sm: 250, md: 800, lg: 1500 },
+              width: { sm: 300, md: 800, lg: 1500 },
               "& .MuiInputBase-root": {
                 height: 65
               }
@@ -45,11 +87,31 @@ function App() {
             onChange={onChangeInput}
           />
         </div>
-        <div className=' flex justify-center py-10'>
+        <div className='flex justify-center py-10'>
           <Button onClick={handleClick} variant="outlined">Generate User Story</Button>
         </div>
-        <div className=' flex justify-center py-10 text-center'>
-          <p>{reply}</p>
+        <div className=' py-10 flex-col'>
+          {reply === '' && isLoading ? (
+            <div className='flex justify-center'>
+              <CircularProgress />
+            </div>
+          ) : (
+            reply !== '' && (
+              <div className='flex justify-center'>
+                <Tooltip title='Copy User Story'>
+                  <IconButton onClick={onCopyClick}>
+                    <ContentCopyIcon />
+                  </IconButton>
+                </Tooltip>
+              </div>
+            )
+          )}
+
+
+
+          <div className='flex justify-center'>
+            <p>{formatReplyWithKeywords(reply)}</p>
+          </div>
         </div>
       </main>
     </ThemeProvider>
